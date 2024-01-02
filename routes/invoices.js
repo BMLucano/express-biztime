5e5c36955de155f53180303e349d3926c7e105ed
Teagan McClintock <teagan.mcclintock@gmail.com>
2024-01-02 10:58:14 -0500

    Added POST and PUT routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -57,7 +57,64 @@ router.get("/:id", async function(req,res){
 
   invoice.company = company;
   return res.json({ invoice });
-})
+});
+
+/**POST /invoices
+ * Adds an invoice.
+ * Needs to be passed in JSON body of: {comp_code, amt}
+ * Returns: {invoice: {id, comp_code, amt, paid, add_date, paid_date}} */
+router.post("/", async function(req, res){
+  if (req.body === undefined
+    || !("comp_code" in req.body)
+    || !("amt" in req.body)
+  ) {
+    throw new BadRequestError("Need to provide valid json");
+  }
+
+  const { comp_code, amt } = req.body;
+
+  try {
+    const results = await db.query(
+      `INSERT INTO invoices (comp_code, amt)
+      VALUES ($1, $2)
+      RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [comp_code, Number(amt)]
+    );
+
+    const invoice = results.rows[0];
+
+    return res.json({ invoice });
+  }
+  catch(err){
+    throw new BadRequestError(); //currently handles comp_code not matching
+  }
+});
+
+/**
+ * PUT /invoices/[id]
+ * Updates an invoice.
+ * If invoice cannot be found, returns a 404.
+ * Needs to be passed in a JSON body of {amt}
+ * Returns: {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+ */
+router.put("/:id", async function(req, res){
+  const id = req.params.id;
+  const { amt } = req.body;
+
+  const results = await db.query(
+    `UPDATE invoices
+    SET amt = $1
+    WHERE id = $2
+    RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+    [Number(amt), id]
+  );
+
+  const invoice = results.rows[0];
+
+  if (!invoice) throw new NotFoundError(`No invoice matching: ${id}`);
+
+  return res.json({ invoice });
+});
 
 
 module.exports = router;
